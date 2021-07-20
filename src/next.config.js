a8749c229288f4ba7e492b8b6c29ae0431a2db29
Venkat.R <ramsunvtech@gmail.com>
2021-07-20 18:28:24 +0800

    tech(base): add next config

---
+++ src/next.config.js
@@ -0,0 +1,10 @@
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+
+  // config options here
+  const nextConfig = {};
+
+  return nextConfig;
+};
