22a5f6bcdcccdb80e0a4368f9458e2b435049a8d
Venkat.R <ramsunvtech@gmail.com>
2021-07-23 14:49:42 +0800

    tech(base): add html index

---
+++ src/pages/blogs/html/index.js
@@ -0,0 +1,17 @@
+// Components.
+import { PostItem } from "../../../common/components/Blog/PostItem";
+import Header from "../../../common/components/Blog/Header";
+
+// Utils.
+import { htmlPosts } from "../../../utils/getPostsByType";
+
+export default function HTMLhubPage() {
+  return (
+    <>
+      <Header title="Webslate.io - HTML5 Articles"/>
+      {htmlPosts.map((post) => (
+        <PostItem type="html" key={post.link} post={post} />
+      ))}
+    </>
+  );
+}
