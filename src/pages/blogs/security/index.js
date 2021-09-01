bb7d1827c5ae901561d82e231100c2e73032ff2f
Venkat.R <ramsunvtech@gmail.com>
2021-09-01 21:26:28 +0800

    docs(security): add main index

--- src/pages/blogs/security/index.js
+++ src/pages/blogs/security/index.js
@@ -5,12 +5,12 @@ import Header from "../../../common/components/Blog/Header";
 // Utils.
 import { securityPosts } from "../../../utils/getPostsByType";
 
-export default function ReactHubPage() {
+export default function SecurityHubPage() {
   return (
     <>
       <Header title="Webslate.io - Security Articles" />
       {securityPosts.map((post) => (
-        <PostItem type="react" key={post.link} post={post} />
+        <PostItem type="security" key={post.link} post={post} />
       ))}
     </>
   );
