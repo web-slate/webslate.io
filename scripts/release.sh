48a5fd6090f2a6f0cc51e3844b2ef6cd77632b49
Venkat.R <ramsunvtech@gmail.com>
2021-10-11 23:03:43 +0800

    Release  1.2.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,19 @@
-echo 'Move to source directoy and Update Project Version.'
-
+echo 'Move to source directoy and do minor Release.'
 # cd src && npm version minor
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
 
 echo 'Build Webslate App'
 # npm run build
 
 echo "Add to Git and Commit for Release version: $PACKAGE_VERSION"
-# cd .. && ga . && gcmsg $PACKAGE_VERSION
+cd .. && git add . && git commit -m "Release $PACKAGE_VERSION"
 
 echo "Create Tag v.$PACKAGE_VERSION"
-# git tag v.$PACKAGE_VERSION
+git tag v.$PACKAGE_VERSION
 
-# git push origin --tags
+git push origin --tags
