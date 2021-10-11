b78a28895700f7bf016470a4a4baf605680bd4f0
Venkat.R <ramsunvtech@gmail.com>
2021-10-11 23:08:16 +0800

    chore(release): update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,15 +5,18 @@ PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
 
 echo 'Build Webslate App'
 # npm run build
 
 echo "Add to Git and Commit for Release version: $PACKAGE_VERSION"
-cd .. && git add . && git commit -m "Release $PACKAGE_VERSION"
+# cd .. && git add . && git commit -m "Release $PACKAGE_VERSION"
 
-echo "Create Tag v.$PACKAGE_VERSION"
+cd ..
+
+# echo "Create Tag v.$PACKAGE_VERSION"
 git tag v.$PACKAGE_VERSION
 
 git push origin --tags
